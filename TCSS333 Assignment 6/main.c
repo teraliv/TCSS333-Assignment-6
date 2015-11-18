818c986537d47981cac14a4da547b69968ef1c88
teraliv <teraliv@gmail.com>
2015-11-18 08:08:59 -0800

    Initial Commit

---
+++ TCSS333 Assignment 6/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TCSS333 Assignment 6
+//
+//  Created by aterikov on 11/18/15.
+//  Copyright © 2015 Alex Terikov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
